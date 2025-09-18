e902c4c88250233b6d7ad351d59e418fd56a80b2
charles jonas <charlie@callawaycloudconsulting.com>
2025-09-18 17:10:31 -0600

    Bump version to 0.1.0b1 (beta release)

--- src/aio_salesforce/__init__.py
+++ src/aio_salesforce/__init__.py
@@ -1,6 +1,6 @@
 """aio-salesforce: Async Salesforce library for Python with Bulk API 2.0 support."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.0b1"
 __author__ = "Jonas"
 __email__ = "charlie@callaway.cloud"
 
